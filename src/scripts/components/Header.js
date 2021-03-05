adea8528284ad9b17030254779010f7d01e40cf1
hyfydistro <hyfydistro@outlook.com>
2021-03-05 17:22:02 +0700

    create header component

---
+++ src/scripts/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// TODO
+// - Add bg image
+// - Responsive Design
+export default function Header() {
+    return (
+        <header>
+            <div className="header-container">
+                <h1>SUPER COMIC SLICER</h1>
+            </div>
+        </header>
+    )
+}
