77587f9f438e05eb62b8a5bdd1b59ff92d156442
hyfydistro <hyfydistro@outlook.com>
2021-03-06 21:51:17 +0700

    create "results" component

---
+++ src/scripts/components/FormResults.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function FormResults() {
+    return (
+        <section className="form-results">
+            <h3>Results</h3>
+
+            <div className="product-container">
+                <div className="product">
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="product__image-block">
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
