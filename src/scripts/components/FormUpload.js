25552f68a71da1a468677018c5f094cabd8043d2
hyfydistro <hyfydistro@outlook.com>
2021-03-11 14:22:11 +0700

    feature thumbnail preview (WIP)

--- src/scripts/components/FormUpload.js
+++ src/scripts/components/FormUpload.js
@@ -13,24 +13,141 @@ const sucessMessages = {
 // - dropzone
 // - preview
 
+// TODO
+// - [ ] roate image function >> on condition
+// i.e. displays WHOLLY (almost) - I don't want cramp
+// horizontal images.
+function Preview(props) {
+
+    const files = props.inputFileRead;
+    const id = [0, 1, 2, 3, 4, 5];
+
+    // ! LOG
+    console.log("INSIDE PREVIEW: ", files);
+
+    if (files) {
+
+        // ! log
+        // console.log("PREVIEW LOG #1: ", files);
+        // console.log("PREVIEW LOG #1: ", files["0"]);
+        if (files.length >= 1) {
+            console.log("PREVIEW LOG #2: ", files["0"]["name"]);
+        }
+
+        const previewThumbnailElements = files.map((file, i) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.addEventListener("load", getDataURL)
+
+            function getDataURL() {
+                console.log("MAKING DATA URL...", reader);
+
+                const currentElement = document.querySelectorAll(".preview__thumbnail-container")[i].lastElementChild.firstElementChild;
+
+                // ! Log
+                console.log(currentElement);
+
+                currentElement.src = reader.result;
+            }
+
+            // ! LOG
+            // console.log("READER: ", reader);
+            // console.log("SOURCE: ", dataURLs);
+            // console.log("SOURCE: 0", dataURLs[0]);
 
 
-export default function FormUpload() {
+            return (
+                <div
+                    key={id[i]}
+                    className="preview__thumbnail-container"
+                    data-label={JSON.stringify(file.name)}
+                >
+                    <button className="close-btn"></button>
+                    <div className="preview__thumbnail">
+                        <img alt="" />
+                    </div>
+                </div>
+            )
+        })
+
+
+        return (
+            <div className="preview-wrapper">
+                <div className="preview-container">
+                    <div className="preview">
+
+                        {previewThumbnailElements}
+
+                        {/* <div className="preview__thumbnail-container" data-label="myfile.txt">
+                            <button className="close-btn"></button>
+                            <div className="preview__thumbnail">
+                                <img src="images/comic2.jpg" alt="" />
+                            </div>
+                        </div>
+
+                        <div className="preview__thumbnail-container" data-label="myfile.txt">
+                            <button className="close-btn"></button>
+                            <div className="preview__thumbnail">
+                                <img src="" alt="" />
+                            </div>
+                        </div> */}
+
+                    </div>
+                    <div className="preview__clear-btn-container">
+                        <button className="preview__clear-btn">Clear Files</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+function Dropzone(props) {
+    return (
+        <div className="dropzone-container">
+            <div
+                className={props.dragOverClass}
+                onDragOver={(e) => props.onHandleDrag(e)}
+                onDragEnter={(e) => props.onHandleDrag(e)}
+                onDragEnd={(e) => props.onHandleDragLeave(e)}
+                onDragLeave={(e) => props.onHandleDragLeave(e)}
+                onDrop={(e) => props.onHandleFileDrop(e)}
+            >
+                <img className="dropzone__upload-icon" src="./images/upload-icon.svg" alt="Drag & Drog Icon" />
+                <p>Drag & Drop or Click here</p>
+                <p className="text-small">to upload your files.(max. 20MB)</p>
+                {/* {props.inputFileElement} */}
+
+                <input onChange={props.onHandleChange} className="dropzone__input" type="file" accept="image/png, image/jpeg" multiple />
+            </div>
+
+        </div>
+    )
+}
+
+
+
+export default function FormUpload(props) {
+    // ! LOG
+    // console.log("INSIDE FORM :", props.previewComponent);
+
     return (
         <section className="form-upload">
 
             <h3>1. Upload</h3>
 
-            <p>Currently, .png and .jpeg (including .jpg) are accepted. </p>
+            <p>Currently, .png and .jpeg (including .jpg) are accepted. If width of image is larger than height, it will be automatically roated. A "rotate" option will be available on next update. 😗</p>
 
-            <div className="dropzone-container">
-                <div className="dropzone">
-                    <img className="dropzone__upload-icon" src="./images/upload-icon.svg" alt="Drag & Drog Icon" />
-                    <p>Drag & Drop or Click here</p>
-                    <p className="text-small">to upload your files.(max. 20MB)</p>
-                    <input type="file" accept="image/png, image/jpeg" multiple />
-                </div>
-            </div>
+            <Dropzone
+                onHandleDrag={props.onHandleDrag}
+                onHandleDragLeave={(e) => props.onHandleDragLeave(e)}
+                dragOverClass={props.dragOverClass}
+                onHandleFileDrop={(e) => props.onHandleFileDrop(e)}
+
+                // inputFileElement={props.inputFileElement}
+                onHandleChange={props.onHandleChange}
+            />
 
             {false
                 ? <div className="alert-message--error">
@@ -43,7 +160,7 @@ export default function FormUpload() {
                 </div>
                 : null}
 
-            {true
+            {false
                 ? <div className="alert-message--success">
                     <img className="alert-icon" src="images/check-icon.svg" alt="" />
                     <span className="alert-message-text--success">
@@ -56,59 +173,22 @@ export default function FormUpload() {
 
             <p>If you have more than one image file, you may drag and re-order.</p>
 
-            <div className="preview-wrapper">
-                <div className="preview-container">
-                    <div className="preview">
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                        <div className="preview__image-block">
-                            <button className="close-btn"></button>
-                            <img src="" alt="" />
-                        </div>
-                    </div>
-                    <div className="preview__clear-btn-container">
-                        <button className="preview__clear-btn">Clear Files</button>
-                    </div>
-                </div>
-            </div>
+            <Preview
+                inputFileRead={props.inputFileRead}
+            />
         </section>
     )
 }
 
 // TODO
-// Create a drop area
-    // - create text
-    // - add image
-// Creat a click to browse
-// Create dragable area
-// use images from upload to preview images
-    // add an event when click on "X" icon it will delete
-        // also update in upload file array
+// [x] Create a drop area
+// [x] Preview Image Thumbnail
+// [x] Preview Image Text
+// [ ] "X" event button to
+// Delete itself from UI and data
+// ? or just update array
+// [ ] Creat a click to browse and
+// add to upload list
+
 // Tools
 // - SortableJS
