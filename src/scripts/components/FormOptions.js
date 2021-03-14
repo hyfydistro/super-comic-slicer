d7e9f47a6c74f5e1664224b086da38e4f5be8aaf
hyfydistro <hyfydistro@outlook.com>
2021-03-15 00:59:35 +0700

    create "options" squash component

--- src/scripts/components/FormOptions.js
+++ src/scripts/components/FormOptions.js
@@ -2,6 +2,49 @@ import React from 'react';
 
 // TODO
 // Create a list component for the following data
+// Squash Level Component
+// Squash Level Component EVENT
+
+function SquashOptions(props) {
+
+    const squashLevelList = props.getSquashLevelModel.map((currentSquashLevel, index) => {
+
+        if (props.getSquashLevelModel[0] == currentSquashLevel) {
+            return (
+                <div className="radio-option" key={index}>
+                    <label htmlFor={currentSquashLevel.htmlLabel}>
+                        <input
+                            defaultChecked
+                            type="radio"
+                            name="squash"
+                            value={currentSquashLevel.pixelValue}
+                            id={currentSquashLevel.htmlLabel}
+                            onClick={(e) => props.onHandleOptionsSquashLevel(e)}
+                        />
+                        <span className="select-text">{currentSquashLevel.text}</span>
+                    </label>
+                </div>
+            )
+        } else {
+            return (
+                <div className="radio-option" key={index}>
+                    <label htmlFor={currentSquashLevel.htmlLabel}>
+                        <input
+                            type="radio"
+                            name="squash"
+                            value={currentSquashLevel.pixelValue}
+                            id={currentSquashLevel.htmlLabel}
+                            onClick={(e) => props.onHandleOptionsSquashLevel(e)}
+                        />
+                        <span className="select-text">{currentSquashLevel.text}</span>
+                    </label>
+                </div>
+            )
+        }
+    });
+
+    return squashLevelList;
+}
 
 function FileExtensionOptions(props) {
 
@@ -11,12 +54,12 @@ function FileExtensionOptions(props) {
                 <div className="radio-option" key={index}>
                     <label htmlFor={fileExtObj.htmlLabel}>
                         <input
-                        defaultChecked
-                        type="radio"
-                        name="filetype"
-                        value={fileExtObj.htmlLabel}
-                        id={fileExtObj.htmlLabel}
-                            onClick={(e)  => props.onHandleOptionsFileExtenions(e)}
+                            defaultChecked
+                            type="radio"
+                            name="filetype"
+                            value={fileExtObj.htmlLabel}
+                            id={fileExtObj.htmlLabel}
+                            onClick={(e) => props.onHandleOptionsFileExtenions(e)}
                         />
                         <span className="select-text">{fileExtObj.text}</span>
                     </label>
@@ -27,11 +70,11 @@ function FileExtensionOptions(props) {
                 <div className="radio-option" key={index}>
                     <label htmlFor={fileExtObj.htmlLabel}>
                         <input
-                        type="radio"
-                        name="filetype"
-                        value={fileExtObj.htmlLabel}
-                        id={fileExtObj.htmlLabel}
-                            onClick={(e)  => props.onHandleOptionsFileExtenions(e)}
+                            type="radio"
+                            name="filetype"
+                            value={fileExtObj.htmlLabel}
+                            id={fileExtObj.htmlLabel}
+                            onClick={(e) => props.onHandleOptionsFileExtenions(e)}
                         />
                         <span className="select-text">{fileExtObj.text}</span>
                     </label>
@@ -63,7 +106,13 @@ export default function FormOptions(props) {
             <p className="margin-fix">Squash scale down the comic page size. If your file size is big enough it may refine illustrations. Slicing ratio will not be affected - it will yield the same page division.</p>
 
             <div className="radio-container-squash">
-                <div className="radio-option">
+
+                <SquashOptions
+                    getSquashLevelModel={props.getSquashLevelModel}
+                    onHandleOptionsSquashLevel={props.onHandleOptionsSquashLevel}
+                />
+
+                {/* <div className="radio-option">
                     <label htmlFor="none">
                         <input type="radio" name="squash" value="" id="none" />
                         <span className="select-text">none</span>
@@ -92,7 +141,7 @@ export default function FormOptions(props) {
                         <input type="radio" name="squash" value="" id="700" />
                         <span className="select-text">700 width pixels</span>
                     </label>
-                </div>
+                </div> */}
             </div>
         </section>
     )
