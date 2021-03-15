b3c59cec754d259fe91958625a47911e58b3a212
hyfydistro <hyfydistro@outlook.com>
2021-03-15 12:26:10 +0700

    remove comment; add more text

--- src/scripts/components/FormOptions.js
+++ src/scripts/components/FormOptions.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-// TODO
-// Create a list component for the following data
-// Squash Level Component
-// Squash Level Component EVENT
-
 function SquashOptions(props) {
 
     const squashLevelList = props.getSquashLevelModel.map((currentSquashLevel, index) => {
@@ -103,45 +98,13 @@ export default function FormOptions(props) {
 
             <p className="text-bold header-four">Squash</p>
 
-            <p className="margin-fix">Squash scale down the comic page size. If your file size is big enough it may refine illustrations. Slicing ratio will not be affected - it will yield the same page division.</p>
+            <p className="margin-fix">Squash scale down the comic page size. If your file size is big enough it may refine illustrations. Slicing ratio will not be affected - it will yield the same page division. However, if your file is smaller than the selected squash option, it will expand your image and attempt to yield to the same aspect ratio of your chosen webcomic platform.</p>
 
             <div className="radio-container-squash">
-
                 <SquashOptions
                     getSquashLevelModel={props.getSquashLevelModel}
                     onHandleOptionsSquashLevel={props.onHandleOptionsSquashLevel}
                 />
-
-                {/* <div className="radio-option">
-                    <label htmlFor="none">
-                        <input type="radio" name="squash" value="" id="none" />
-                        <span className="select-text">none</span>
-                    </label>
-                </div>
-                <div className="radio-option">
-                    <label htmlFor="400">
-                        <input type="radio" name="squash" value="" id="400" />
-                        <span className="select-text">400 width pixels</span>
-                    </label>
-                </div>
-                <div className="radio-option">
-                    <label htmlFor="500">
-                        <input type="radio" name="squash" value="" id="500" />
-                        <span className="select-text">500 width pixels</span>
-                    </label>
-                </div>
-                <div className="radio-option">
-                    <label htmlFor="600">
-                        <input type="radio" name="squash" value="" id="600" />
-                        <span className="select-text">600 width pixels</span>
-                    </label>
-                </div>
-                <div className="radio-option">
-                    <label htmlFor="700">
-                        <input type="radio" name="squash" value="" id="700" />
-                        <span className="select-text">700 width pixels</span>
-                    </label>
-                </div> */}
             </div>
         </section>
     )
