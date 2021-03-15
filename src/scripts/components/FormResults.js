5a7b34acd811ba4db8b111ef3879f851b079f3f5
hyfydistro <hyfydistro@outlook.com>
2021-03-15 15:56:03 +0700

    create "Product" component

--- src/scripts/components/FormResults.js
+++ src/scripts/components/FormResults.js
@@ -1,34 +1,35 @@
 import React from 'react'
 
-export default function FormResults() {
+// TODO: FEATURE (NEXT UPDATE)
+// adjust to webcomic platform ratio for display
+
+function Product(props) {
+
+    // INJECT image blob
+    const processedImageFIles = props.getImageData.map((imgData, index) => {
+
+        return (
+            <div className="product__image-block" key={index}>
+                <img src={imgData} />
+            </div>
+        )
+    })
+
+    return processedImageFIles;
+}
+
+export default function FormResults(props) {
     return (
         <section className="form-results">
             <h3>Results</h3>
 
             <canvas id="canvas"></canvas>
+
             <div className="product-container">
                 <div className="product">
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="product__image-block">
-                        <img src="" alt="" />
-                    </div>
+                    <Product
+                        getImageData={props.getImageData}
+                    />
                 </div>
             </div>
         </section>
