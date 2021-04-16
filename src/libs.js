901b7ad694f7b3e3d168df2e75d60029f4661030
hyfydistro <hyfydistro@outlook.com>
2021-04-16 14:05:13 +0700

    feat: add service worker with "workbox"

--- src/libs.js
+++ src/libs.js
@@ -4,3 +4,14 @@
 // libs
 import createId from './scripts/libs/createId.js';
 import convertBytes from './scripts/libs/convertBytes.js';
+
+// Service Worker
+if ("serviceWorker" in navigator) {
+  // * console.log("SW is supported");
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration ) => console.log("SW registered: ", registration))
+      .catch((registrationError ) => console.log("SW registration failed: ", registrationError));
+  });
+}
