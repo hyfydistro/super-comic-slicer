90812afb9b5c0dffc324acafbb0f8f6ff39f184f
hyfydistro <hyfydistro@outlook.com>
2021-04-16 13:59:37 +0700

    refactor(images): add new edited image

--- src/assets.js
+++ src/assets.js
@@ -1,26 +1,25 @@
 // =====
 // Files
 // =====
+// Fonts
+import "./fonts/icomoon.eot";
+import "./fonts/icomoon.ttf";
+import "./fonts/icomoon.woff";
+import "./images/icomoon.svg";
+import "./fonts/quicksand/Quicksand-Bold.ttf";
+import "./fonts/quicksand/Quicksand-Regular.ttf";
+import "./fonts/josefin-sans/JosefinSans-SemiBold.ttf";
+// Images
 import "./images/header-bg.png";
 import "./images/salespitch.svg";
 import "./images/webtoon-icon.svg";
 import "./images/tapas-icon.png";
 import "./images/bmc-btn.png";
 import "./images/og-supercomicslicer.png";
-import "./images/load-hamster.gif";
 // Favicon
 import "./images/favicon-32x32.png";
 import "./images/apple-touch-icon.png";
 import "./favicon.ico";
 // Webmanifest file
 import "./site.webmanifest";
-// Fonts
-import "./fonts/icomoon.eot";
-import "./fonts/icomoon.ttf";
-import "./fonts/icomoon.woff";
-import "./images/icomoon.svg";
 
-// ======
-// Styles
-// ======
-import "./main.styles.scss";
