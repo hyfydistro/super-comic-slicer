236c795debe88a7a7f6315fdbe04af1b33c2f800
Caroline Rozali <hyfydistro@outlook.com>
2024-01-18 14:38:04 +0700

    build: add eslint favoritemedium config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  extends: ['favoritemedium-typescript/config/react'],  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+}
